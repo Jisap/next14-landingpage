fc18a3ed8966fcd47decd3de6b1d28be1218191b
Jorge Matos <jisapjisap@gmail.com>
2024-07-22 13:03:03 +0200

    ProductShowcase

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,26 @@
+
+
+import productImage from '@/assets/product-image.png'
+import Image from 'next/image';
+
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <section className='bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24'>
+      <div className="container">
+        <div>
+          Boost your productivity
+        </div>
+        <h2>A more effective way to track progress</h2>
+        <p>
+          Effortlessly turn your ideas into a fully functional, responsive, SaaS
+          website in just minutes with this template.
+        </p>
+        <Image 
+          src={productImage}
+          alt="product image"
+          className=''
+        />
+      </div>
+    </section>
+  )
 };
