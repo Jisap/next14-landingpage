cd138df71e6ad10d4831f75ae485eb28601756a6
Jorge Matos <jisapjisap@gmail.com>
2024-07-22 13:15:22 +0200

    ProductShowcase

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -7,11 +7,15 @@ export const ProductShowcase = () => {
   return (
     <section className='bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24'>
       <div className="container">
-        <div>
-          Boost your productivity
+        <div className='flex justify-center'>
+          <div className='tag'>
+            Boost your productivity
+          </div>
         </div>
-        <h2>A more effective way to track progress</h2>
-        <p>
+        <h2 className='text-center text-3xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5'>
+          A more effective way to track progress
+        </h2>
+        <p className='text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5'>
           Effortlessly turn your ideas into a fully functional, responsive, SaaS
           website in just minutes with this template.
         </p>
