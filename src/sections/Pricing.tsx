c13cc718db153de38719f7ad6ab6a0e5fd89a6b0
Jorge Matos <jisapjisap@gmail.com>
2024-07-22 13:55:25 +0200

    Pricing

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -52,14 +52,19 @@ const pricingTiers = [
 
 export const Pricing = () => {
   return (
-    <section>
+    <section className="py-24">
       <div className="container">
         <h2 className="section-title">
           Pricing
         </h2>
-        <p className="section-description">
+        <p className="section-description mt-5">
           Free forever. Upgrade for unlimited tasks, better security, and exclusive features.
         </p>
+        <div>
+          <div>
+            <h3></h3>
+          </div>
+        </div>
       </div>
     </section>
   )
