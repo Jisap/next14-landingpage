761bda43095c99447edc8e2d036f5dfa46a1489a
Jorge Matos <jisapjisap@gmail.com>
2024-07-22 13:34:17 +0200

    ProductShowcase

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,11 +1,13 @@
 
 
 import productImage from '@/assets/product-image.png'
+import pyramideImage from '@/assets/pyramid.png'
+import tube from '@/assets/tube.png'
 import Image from 'next/image';
 
 export const ProductShowcase = () => {
   return (
-    <section className='bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24'>
+    <section className='bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip'>
       <div className="container">
         <div className='max-w-[540px] mx-auto'>
           <div className='flex justify-center'>
@@ -13,7 +15,7 @@ export const ProductShowcase = () => {
               Boost your productivity
             </div>
           </div>
-          <h2 className='text-center text-3xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5'>
+          <h2 className='text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5'>
             A more effective way to track progress
           </h2>
           <p className='text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5'>
@@ -21,11 +23,26 @@ export const ProductShowcase = () => {
             website in just minutes with this template.
           </p>
         </div>
-        <Image
-          src={productImage}
-          alt="product image"
-          className='mt-10'
-        />
+        <div className='relative'>
+          <Image
+            src={productImage}
+            alt="product image"
+            className='mt-10'
+          />
+          <Image 
+            src={pyramideImage}
+            alt='Pyramid Image'
+            height={262}
+            width={262}
+            className='hidden md:block absolute -right-36 -top-32'
+          />
+          <Image 
+            src={tube}
+            alt='Tube Image'
+            height={248}
+            className='hidden md:block absolute bottom-24 -left-36'
+          />
+        </div>
       </div>
     </section>
   )
