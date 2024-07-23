6e41911b11775928c71df5f4893607baacf6f8dc
Jorge Matos <jisapjisap@gmail.com>
2024-07-23 19:07:09 +0200

    pricing

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -64,16 +64,29 @@ export const Pricing = () => {
         </p>
         <div>
           {pricingTiers.map(({title, monthlyPrice, buttonText, popular, inverse, features}) => (
+  
             <div className='p-10 rounded-3xl shadow-[0_7px_14px_#EAEAEA] border border-[#F1F1F1]'>
-              <h3 className='text-lg font-bold text-black/50'>{title}</h3>
-              <div className='flex items-baseline gap-1'>
+              <div className='flex justify-between'>
+                <h3 className='text-lg font-bold text-black/50'>{title}</h3>
+                {popular && (
+                  <div className='inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20'>
+                    <span className='bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] text-transparent bg-clip-text font-medium'>
+                      Popular
+                    </span>
+                  </div>
+                )}
+              </div>
+              <div className='flex items-baseline gap-1 mt-[30px]'>
                 <span className='text-4xl font-bold tracking-tighter leading-none'>${monthlyPrice}</span>
                 <span className='tracking-tight font-bold text-black/50'>/month</span>
               </div>
-              <button>{buttonText}</button>
-              <ul>
+              <button className='btn btn-primary w-full mt-[30px]'>{buttonText}</button>
+              <ul className='flex flex-col gap-5 mt-8'>
                 {features.map((feature, index) => (
-                  <li key={index}>
+                  <li
+                    className='text-sm flex items-center gap-4' 
+                    key={index}
+                  >
                     <CheckIcon className="h-6 w-6"/>
                     <span></span>{feature}
                   </li>
