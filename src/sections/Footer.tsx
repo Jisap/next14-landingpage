30b5c25a1abfb99f0765dc8aa1e73fe3a043f9dd
Jorge Matos <jisapjisap@gmail.com>
2024-07-25 16:38:47 +0200

    footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -18,7 +18,7 @@ export const Footer = () => {
             className='relative'
           />
         </div>
-        <nav>
+        <nav className='flex flex-col md:flex-row md:justify-center gap-6 mt-6'>
           <a href="#">About</a>
           <a href="#">Features</a>
           <a href="#">Customers</a>
@@ -26,14 +26,14 @@ export const Footer = () => {
           <a href="#">Help</a>
           <a href="#">Careers</a>
         </nav>
-        <div>
+        <div className='flex justify-center gap-6 mt-6'>
           <SocialX />
           <SocialInsta />
           <SocialLinkeding />
           <SocialPin />
           <SocialYoutube />
         </div>
-        <p>
+        <p className='mt-6'>
           &copy; 2024 Your Company, Inc. All rights reserved.
         </p>
       </div>
