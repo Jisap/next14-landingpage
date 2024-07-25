888745c3ae69b3ef56699bbb0cff04c772cce38b
Jorge Matos <jisapjisap@gmail.com>
2024-07-25 16:33:35 +0200

    footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,17 +1,23 @@
 import logo from '@/assets/logosaas.png'
 import Image from 'next/image';
-
+import SocialX from '@/assets/social-x.svg'
+import SocialInsta from '@/assets/social-insta.svg'
+import SocialLinkeding from '@/assets/social-linkedin.svg'
+import SocialPin from '@/assets/social-pin.svg'
+import SocialYoutube from '@/assets/social-youtube.svg'
 
 export const Footer = () => {
   return (
-    <footer>
+    <footer className='bg-black text-[#BCBCBC] text-sm py-10 text-center'>
       <div className="container">
-        <Image
-          src={logo}
-          alt="logo"
-          height={40}
-          className=''
-        />
+        <div className="inline-flex relative before:content-[''] before:top-2 before:bottom-0 before:h-full before:blur before:w-full before:bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F0B1,#2FD8FE)] before:absolute">
+          <Image
+            src={logo}
+            alt="logo"
+            height={40}
+            className='relative'
+          />
+        </div>
         <nav>
           <a href="#">About</a>
           <a href="#">Features</a>
@@ -20,6 +26,16 @@ export const Footer = () => {
           <a href="#">Help</a>
           <a href="#">Careers</a>
         </nav>
+        <div>
+          <SocialX />
+          <SocialInsta />
+          <SocialLinkeding />
+          <SocialPin />
+          <SocialYoutube />
+        </div>
+        <p>
+          &copy; 2024 Your Company, Inc. All rights reserved.
+        </p>
       </div>
     </footer>
   )
