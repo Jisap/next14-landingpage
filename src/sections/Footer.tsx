eca7b36fa19c160376ac765e002e48bc4be9c941
Jorge Matos <jisapjisap@gmail.com>
2024-07-25 16:17:00 +0200

    footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,26 @@
+import logo from '@/assets/logosaas.png'
+import Image from 'next/image';
+
+
 export const Footer = () => {
-  return null;
+  return (
+    <footer>
+      <div className="container">
+        <Image
+          src={logo}
+          alt="logo"
+          height={40}
+          className=''
+        />
+        <nav>
+          <a href="#">About</a>
+          <a href="#">Features</a>
+          <a href="#">Customers</a>
+          <a href="#">Pricing</a>
+          <a href="#">Help</a>
+          <a href="#">Careers</a>
+        </nav>
+      </div>
+    </footer>
+  )
 };
