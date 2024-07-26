27fa1b17cfab94797e7ab4a98b4917f9fb6917d3
Jorge Matos <jisapjisap@gmail.com>
2024-07-26 15:39:17 +0200

    hero animations

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,11 @@
-
+"use client"
 
 import ArrowIcon from '@/assets/arrow-right.svg'
 import cogImage from '@/assets/cog.png'
 import cylinderImg from '@/assets/cylinder.png'
 import noodleImage from '@/assets/noodle.png'
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 
 export const Hero = () => {
@@ -38,10 +39,18 @@ export const Hero = () => {
           </div>
 
           <div className='mt-20 md:mt-0 md:h-[648px] md:flex-1 relative'>
-            <Image
-              src={cogImage}
+            <motion.img
+              src={cogImage.src}
               alt="cogImage"
               className='md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0'
+              animate={{
+                translateY: [-30, 30]
+              }}
+              transition={{
+                repeat: Infinity,
+                repeatType: "mirror",
+                duration: 5,
+              }}
             />
             <Image 
               src={cylinderImg}
