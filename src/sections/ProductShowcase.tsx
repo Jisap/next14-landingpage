1bbd6cc3867aac80963fb3bc825c7c6a906d164f
Jorge Matos <jisapjisap@gmail.com>
2024-07-26 17:14:16 +0200

    ProductShowcase animations

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,13 +1,30 @@
-
+"use client"
 
 import productImage from '@/assets/product-image.png'
 import pyramideImage from '@/assets/pyramid.png'
 import tube from '@/assets/tube.png'
+import { motion, useScroll, useTransform } from 'framer-motion';
 import Image from 'next/image';
+import { useRef } from 'react';
+
+
 
 export const ProductShowcase = () => {
+
+  const sectionRef = useRef(null)
+
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ['start end', 'end start']
+  });
+
+  const translateY = useTransform(scrollYProgress, [0,1], [150, -150])
+
   return (
-    <section className='bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip'>
+    <section 
+      ref={sectionRef}
+      className='bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip'
+    >
       <div className="container">
         <div className='section-heading'>
           <div className='flex justify-center'>
@@ -24,23 +41,30 @@ export const ProductShowcase = () => {
           </p>
         </div>
         <div className='relative'>
-          <Image
-            src={productImage}
+          <motion.img
+            src={productImage.src}
             alt="product image"
             className='mt-10'
           />
-          <Image 
-            src={pyramideImage}
+          <motion.img 
+            src={pyramideImage.src}
             alt='Pyramid Image'
             height={262}
             width={262}
             className='hidden md:block absolute -right-36 -top-32'
+            style={{
+              translateY,
+            }}
           />
-          <Image 
-            src={tube}
+          <motion.img 
+            src={tube.src}
             alt='Tube Image'
             height={248}
+            width={248}
             className='hidden md:block absolute bottom-24 -left-36'
+            style={{
+              translateY,
+            }}
           />
         </div>
       </div>
