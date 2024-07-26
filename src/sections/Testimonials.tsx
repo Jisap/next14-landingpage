eed994bceec4c8052e860db65f8597ad63b1bcc4
Jorge Matos <jisapjisap@gmail.com>
2024-07-26 18:57:29 +0200

    Testimonials

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
@@ -7,8 +9,12 @@ import avatar6 from "@/assets/avatar-6.png";
 import avatar7 from "@/assets/avatar-7.png";
 import avatar8 from "@/assets/avatar-8.png";
 import avatar9 from "@/assets/avatar-9.png";
+import { motion } from "framer-motion";
 import Image from "next/image";
+import React from "react";
 import { twMerge } from "tailwind-merge";
+
+
 
 const testimonials = [
   {
@@ -71,29 +77,47 @@ const firstColumn = testimonials.slice(0, 3)
 const secondColumn = testimonials.slice(3, 6)
 const thirdColumn = testimonials.slice(6, 9)
 
-const TextimonialsColumns = (props: { className?: string; testimonials: typeof testimonials }) => (
-  <div className={twMerge(
-    "flex flex-col gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]",
-    props.className
-  )}>
-    {props.testimonials.map(({ text, imageSrc, name, username }) => (
-      <div className="card">
-        <div>{text}</div>
-        <div className="flex items-center gap-2 mt-5">
-          <Image
-            src={imageSrc}
-            alt={name}
-            width={40}
-            height={40}
-            className="h-10 w-10 rounde-full"
-          />
-          <div className="flex flex-col">
-            <div className="font-medium tracking-tight leading-5">{name}</div>
-            <div className="leading-5 tracking-tight">{username}</div>
-          </div>
-        </div>
-      </div>
-    ))}
+const TextimonialsColumns = (props: { 
+  className?: string; 
+  testimonials: typeof testimonials;
+  duration?: number; 
+}) => (
+  <div className={props.className}>
+    <motion.div 
+      className="flex flex-col gap-6 pb-6"
+      animate={{
+        translateY: "-50%",
+      }}
+      transition={{
+        duration: props.duration || 10,
+        repeat: Infinity,
+        ease: "linear",
+        repeatType: "loop",
+      }}
+    >
+      {[...new Array(2)].fill(0).map((_, index) => (
+        <React.Fragment key={index}>
+          {props.testimonials.map(({ text, imageSrc, name, username }) => ( // Devuelve 2 veces las props 
+            <div className="card">
+              <div>{text}</div>
+              <div className="flex items-center gap-2 mt-5">
+                <Image
+                  src={imageSrc}
+                  alt={name}
+                  width={40}
+                  height={40}
+                  className="h-10 w-10 rounde-full"
+                />
+                <div className="flex flex-col">
+                  <div className="font-medium tracking-tight leading-5">{name}</div>
+                  <div className="leading-5 tracking-tight">{username}</div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </React.Fragment>
+      ))}
+    </motion.div>
   </div>
 )
 
@@ -112,10 +136,10 @@ export const Testimonials = () => {
             From intuitive design to powerful features, our app has become an essential tool for users around the world.
           </p>
         </div>
-        <div className="flex justify-center gap-6">
-          <TextimonialsColumns testimonials={firstColumn} />
-          <TextimonialsColumns testimonials={secondColumn} className="hidden md:flex" />
-          <TextimonialsColumns testimonials={thirdColumn} className="hidden lg:flex" />
+        <div className="flex justify-center gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)] max-h-[738px] overflow-hidden">
+          <TextimonialsColumns testimonials={firstColumn} duration={15} />
+          <TextimonialsColumns testimonials={secondColumn} duration={19} className="hidden md:block" />
+          <TextimonialsColumns testimonials={thirdColumn} duration={17} className="hidden lg:block" />
         </div>
       </div>
     </section>
